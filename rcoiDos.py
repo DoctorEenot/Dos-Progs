0b26dfc34fa29f72872006d650300d242cff06d6
DoctorEenot <47860375+DoctorEenot@users.noreply.github.com>
2020-01-30 20:32:22 +0200

    Update rcoiDos.py

--- rcoiDos.py
+++ rcoiDos.py
@@ -29,15 +29,15 @@ def dos():
                 break
 
 
+if __name__ == '__main__':
+    t = []
+    n = 500
 
-t = []
-n = 500
+    for i in range(n):
+            t.append(multiprocessing.Process(target = dos))
 
-for i in range(n):
-        t.append(multiprocessing.Process(target = dos))
+    for i in range(n):
+            t[i].start()
 
-for i in range(n):
-        t[i].start()
-
-for i in range(n):
-        t[i].join()
+    for i in range(n):
+            t[i].join()
